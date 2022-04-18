e605a8b3a39150c90aeca5d13321efc194ef6da3
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-18 11:44:21 +0530

    Death Of  All Zombies Player Will Dance

---
+++ Assets/MidLevelFPSGame/MidLevelFPSGame-master/Assets/SpawnManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] zombiePrefabs;
+    public int number;
+    public float spawnRadius;
+    public bool spawnOnStart = true;
+    Vector3 result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnOnStart)
+        {
+            CreateAllZombies();
+        }
+        
+    }
+
+    private void CreateAllZombies()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            print("Transform.position = "  +transform.position);
+            Vector3 randomPoint = transform.position + Random.insideUnitSphere * spawnRadius;
+            print("Random point = " +randomPoint);
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 10f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                //print("Result=" +result);
+                Instantiate(zombiePrefabs[0], result, Quaternion.identity);
+            }
+            else
+                i--;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!spawnOnStart  && other.gameObject.tag == "Player")
+        {
+            CreateAllZombies();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+}
